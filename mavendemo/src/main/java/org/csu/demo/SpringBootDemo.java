e79c86cb084648b2e6e8a169da7e5b65f00ecdee
Lee-VA <a123213090@126.com>
2020-03-19 15:02:38 +0800

    SpringBoot
    使用springboot完成了helloworld

---
+++ mavendemo/src/main/java/org/csu/demo/SpringBootDemo.java
@@ -0,0 +1,15 @@
+package org.csu.demo;
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootDemo.class, args);
+
+    }
+
+}
