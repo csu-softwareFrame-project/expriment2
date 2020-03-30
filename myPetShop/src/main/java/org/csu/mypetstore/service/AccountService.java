c27b6b34617f5286132cd823122c99a2a92f9a12
@zhou_jie12821948 <1670514771@qq.com>
2020-03-31 00:04:51 +0800

    完成Account部分的数据库代码

--- myPetShop/src/main/java/org/csu/mypetstore/service/AccountService.java
+++ myPetShop/src/main/java/org/csu/mypetstore/service/AccountService.java
@@ -1,7 +1,7 @@
 package org.csu.mypetstore.service;
 
 import org.csu.mypetstore.domain.Account;
-import org.csu.mypetstore.persistence.AccountDAO;
+import org.csu.mypetstore.persistence.AccountMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +9,31 @@ import org.springframework.stereotype.Service;
 public class AccountService {
 
     @Autowired
-    private AccountDAO accountDAO;
+    private AccountMapper accountMapper;
 
-    public Account login(Account account){
-        return accountDAO.findByUsername(account);
+    public Account getAccount(String username) {
+        return accountMapper.getAccountByUsername(username);
+    }
+
+    public Account getAccount(String username, String password) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        return accountMapper.getAccountByUsernameAndPassword(account);
+    }
+
+    public void insertAccount(Account account) {
+        accountMapper.insertAccount(account);
+        accountMapper.insertProfile(account);
+        accountMapper.insertSignon(account);
+    }
+
+    public void updateAccount(Account account) {
+        accountMapper.updateAccount(account);
+        accountMapper.updateProfile(account);
+
+        if (account.getPassword() != null && account.getPassword().length() > 0) {
+            accountMapper.updateSignon(account);
+        }
     }
 }
