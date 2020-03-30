ca032a5d86cf657b9ece56c6cf53e4a148907779
@zhou_jie12821948 <1670514771@qq.com>
2020-03-31 00:04:51 +0800

    完成Account部分的数据库代码

--- myPetShop/src/main/java/org/csu/mypetstore/service/AccountService.java
+++ myPetShop/src/main/java/org/csu/mypetstore/service/AccountService.java
@@ -11,7 +11,29 @@ public class AccountService {
     @Autowired
     private AccountMapper accountMapper;
 
-    public Account login(Account account){
-        return accountMapper.findByUsername(account);
+    public Account getAccount(String username) {
+        return accountMapper.getAccountByUsername(username);
+    }
+
+    public Account getAccount(String username, String password) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        return accountMapper.getAccountByUsernameAndPassword(account);
+    }
+
+    public void insertAccount(Account account) {
+        accountMapper.insertAccount(account);
+        accountMapper.insertProfile(account);
+        accountMapper.insertSignon(account);
+    }
+
+    public void updateAccount(Account account) {
+        accountMapper.updateAccount(account);
+        accountMapper.updateProfile(account);
+
+        if (account.getPassword() != null && account.getPassword().length() > 0) {
+            accountMapper.updateSignon(account);
+        }
     }
 }
