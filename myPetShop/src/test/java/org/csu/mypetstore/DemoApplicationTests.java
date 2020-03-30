e9abe8dfe8fbf220e93287e8d9d84bd7eb0d8bfb
@zhou_jie12821948 <1670514771@qq.com>
2020-03-31 00:04:51 +0800

    完成Account部分的数据库代码

--- myPetShop/src/test/java/org/csu/mypetstore/DemoApplicationTests.java
+++ myPetShop/src/test/java/org/csu/mypetstore/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package org.csu.mypetstore;
 
+import org.csu.mypetstore.domain.Account;
 import org.csu.mypetstore.domain.Category;
 import org.csu.mypetstore.domain.Item;
 import org.csu.mypetstore.domain.Product;
@@ -111,5 +112,71 @@ class DemoApplicationTests {
     void testUpdateQuantity()
     {
         catalogService.updateInventoryQuantity(20,"EST-10");
+    }
+
+    @Test
+    void testAccountByUsername()
+    {
+        Account account = accountService.getAccount("233");
+        System.out.println(account.getUsername() + "," + account.getLanguagePreference());
+        System.out.println(account.isListOption());
+    }
+
+    @Test
+    void testAccountByUsernameAndPassword()
+    {
+        Account account = accountService.getAccount("233","123");
+        System.out.println(account.getLanguagePreference());
+    }
+
+    @Test
+    void testAccountInsert()
+    {
+        Account account = new Account();
+        account.setUsername("wofule");
+        account.setPassword("abaaba");
+        account.setLanguagePreference("Chinese");
+        account.setEmail("777");
+        account.setFirstName("Z");
+        account.setLastName("J");
+        account.setStatus("55");
+        account.setBannerOption(true);
+        account.setListOption(false);
+        account.setFavouriteCategoryId("BIRDS");
+        account.setCountry("China");
+        account.setZip("FJ");
+        account.setState("FZ");
+        account.setCity("FZ");
+        account.setAddress2("FZYZ");
+        account.setAddress1("CSU");
+        account.setPhone("8848");
+        account.setBannerName("CATS");
+        accountService.insertAccount(account);
+    }
+
+    @Test
+    void testAccountUpdate()
+    {
+        Account account = new Account();
+        account.setUsername("hahaha");
+        account.setPassword("999");
+        account.setLanguagePreference("Chinese");
+        account.setEmail("888");
+        account.setFirstName("Z");
+        account.setLastName("J");
+        account.setStatus("55");
+        account.setBannerOption(false);
+        account.setListOption(false);
+        account.setFavouriteCategoryId("BIRDS");
+        account.setCountry("China");
+        account.setZip("FJ");
+        account.setState("FZ");
+        account.setCity("FZ");
+        account.setAddress2("FZYZ");
+        account.setAddress1("CSU");
+        account.setPhone("8848");
+        account.setBannerName("CATS");
+
+        accountService.updateAccount(account);
     }
 }
