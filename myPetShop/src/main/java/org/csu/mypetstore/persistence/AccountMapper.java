ac9b42627073c057161e018dc6c323c5e5f3608b
@zhou_jie12821948 <1670514771@qq.com>
2020-03-31 00:05:22 +0800

    完成Account部分的数据库代码2

--- myPetShop/src/main/java/org/csu/mypetstore/persistence/AccountMapper.java
+++ myPetShop/src/main/java/org/csu/mypetstore/persistence/AccountMapper.java
@@ -5,6 +5,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountMapper {
+    Account getAccountByUsername(String username);
 
-    Account findByUsername(Account account);
+    Account getAccountByUsernameAndPassword(Account account);
+
+    void insertAccount(Account account);
+
+    void insertProfile(Account account);
+
+    void insertSignon(Account account);
+
+    void updateAccount(Account account);
+
+    void updateProfile(Account account);
+
+    void updateSignon(Account account);
 }
