1e2e4d5da1a946f6cb15fb489cd4b4c6a3f4d647
@zhou_jie12821948 <1670514771@qq.com>
2020-03-31 11:59:03 +0800

    完成数据库基本功能

--- myPetShop/src/main/java/org/csu/mypetstore/domain/CartItem.java
+++ myPetShop/src/main/java/org/csu/mypetstore/domain/CartItem.java
@@ -4,54 +4,54 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 public class CartItem implements Serializable {
-  private static final long serialVersionUID = 6620528781626504362L;
 
-  private Item item;
-  private int quantity;
-  private boolean inStock;
-  private BigDecimal total;
+    private static final long serialVersionUID = 6620528781626504362L;
 
-  public boolean isInStock() {
-    return inStock;
-  }
+    private Item item;
+    private int quantity;
+    private boolean inStock;
+    private BigDecimal total;
 
-  public void setInStock(boolean inStock) {
-    this.inStock = inStock;
-  }
-
-  public BigDecimal getTotal() {
-    return total;
-  }
-
-  public Item getItem() {
-    return item;
-  }
-
-  public void setItem(Item item) {
-    this.item = item;
-    calculateTotal();
-  }
-
-  public int getQuantity() {
-    return quantity;
-  }
-
-  public void setQuantity(int quantity) {
-    this.quantity = quantity;
-    calculateTotal();
-  }
-
-  public void incrementQuantity(int quantity) {
-    this.quantity=this.quantity+quantity;
-    calculateTotal();
-  }
-
-  private void calculateTotal() {
-    if (item != null && item.getListPrice() != null) {
-      total = item.getListPrice().multiply(new BigDecimal(quantity));
-    } else {
-      total = null;
+    public boolean isInStock() {
+        return inStock;
     }
-  }
 
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+        calculateTotal();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        calculateTotal();
+    }
+
+    public void incrementQuantity() {
+        quantity++;
+        calculateTotal();
+    }
+
+    private void calculateTotal() {
+        if (item != null && item.getListPrice() != null) {
+            total = item.getListPrice().multiply(new BigDecimal(quantity));
+        } else {
+            total = null;
+        }
+    }
 }
