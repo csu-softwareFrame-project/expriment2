29e6aee4df84c6a92566d25979370469bfbc3537
Lee-VA <a123213090@126.com>
2020-04-02 22:48:34 +0800

    完善验证码功能
    邮件验证码，外加修改了editAccount，其中验证码的判断需要补充写完，相关注释已经写好。

--- myPetShop/src/main/resources/static/js/averify.js
+++
@@ -1,9 +0,0 @@
-// function averify() {
-//     var input = document.getElementById("verifyCode")
-//     var result = document.getElementById("verifyResult")
-//     result.innerText = aaa;
-//     getWord();
-//     function getWord(){
-//         result.innerText = "aaaa";
-//     }
-// }
