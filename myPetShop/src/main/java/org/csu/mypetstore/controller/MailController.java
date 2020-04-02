14c3331be8e41e0cd1fa910804fd715af6d1196d
Lee-VA <a123213090@126.com>
2020-04-02 23:46:22 +0800

    Revert "Revert "完善验证码功能""
    This reverts commit 414f9d10b0a5c786d2ee643c2ddc88541d7647c1.

---
+++ myPetShop/src/main/java/org/csu/mypetstore/controller/MailController.java
@@ -0,0 +1,33 @@
+package org.csu.mypetstore.controller;
+
+import org.csu.mypetstore.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.Random;
+
+@Controller
+@RequestMapping("/mail")
+public class MailController {
+    @Autowired
+    private MailService mailService;
+
+    @RequestMapping("/getCheckCode")
+    @ResponseBody
+    public String getCheckCode(String email, Model model, HttpSession session){
+        String checkCode = String.valueOf(new Random().nextInt(899999) + 100000);
+        session.setAttribute("checkCode", checkCode);
+        String message = "您的登陆验证码为："+checkCode;
+        model.addAttribute("checkCode", checkCode);
+        try {
+            mailService.sendSimpleMail(email, "登陆验证码", message);
+        }catch (Exception e){
+            return "";
+        }
+        return checkCode;
+    }
+}
