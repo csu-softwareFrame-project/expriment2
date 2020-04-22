15714d1d46c5c818db9d87cca718ed2f9a44f680
Lee-VA <a123213090@126.com>
2020-04-22 21:38:12 +0800

    首页改动样式

--- myPetStore/myPetStore-front/src/components/Hello.vue
+++ myPetStore/myPetStore-front/src/components/Hello.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="hello">
+  <div id="hello" :style ="note">
+    <br><br><br><br><br><br><br><br>
     <div class="wrap">   <!--最大的空间div-->
       <div class="cube">   <!--正方体的box-->
         <!--这个是大的正方体的六面-->
-        <div class="out-front"><router-link to="/main/view-main"><div><font color="aqua">welcome</font></div></router-link></div>
-        <div class="out-back"><router-link to="/main/view-main"><div><font color="aqua">mypetstore</font></div></router-link></div>
-        <div class="out-left"><router-link to="/main/view-main"><div><font color="aqua">enter</font></div></router-link></div>
-        <div class="out-right"><a href="../../static/help.html"><div><font color="#7fff00">help</font></div></a></div>
-        <div class="out-top"><router-link to="/main/view-main"><div><font color="aqua">(^-^)</font></div></router-link></div>
-        <div class="out-bottom"><router-link to="/main/view-main"><div><font color="aqua">to</font></div></router-link></div>
+        <div class="out-front"><router-link to="/main/view-main"><div><font color="#faebd7">welcome</font></div></router-link></div>
+        <div class="out-back"><router-link to="/main/view-main"><div><font color="#faebd7">mypetstore</font></div></router-link></div>
+        <div class="out-left"><router-link to="/main/view-main"><div><font color="#faebd7">enter</font></div></router-link></div>
+        <div class="out-right"><a href="../../static/help.html"><div><font color="#faebd7">help</font></div></a></div>
+        <div class="out-top"><router-link to="/main/view-main"><div><font color="#faebd7">(^-^)</font></div></router-link></div>
+        <div class="out-bottom"><router-link to="/main/view-main"><div><font color="#faebd7">to</font></div></router-link></div>
 
         <!--这个是小的正方体的六面-->
         <span class="in-front">             <!--前面的点数1-->
@@ -50,14 +51,14 @@
 
       </div>
     </div>
-    <br><br><br><br><br><br><br><br>
+    <br><br><br><br><br>
      <div class="btn-group">
        <div class="bottom">
          <div>
-           <div class="admin5"><router-link to="/main/view-main"><b><font color="aqua" size="5px">click here</font></b></router-link></div>
+           <div class="admin5"><router-link to="/main/view-main"><b><font color="#faebd7" size="5px">click here</font></b></router-link></div>
          </div>
          <div>
-           <font color="#8fbc8f" size="3px">or the dice to enter mypetstore</font>
+           <font color="#faebd7" size="3px">or the dice to enter mypetstore</font>
          </div>
        </div>
      </div>
@@ -65,11 +66,23 @@
     <div>
       <router-link to="/management/mainPage"><font color="#dc143c" size="5px">test</font></router-link>
     </div>
+    <br><br><br>
   </div>
 </template>
 
 <script>
-
+export default {
+  data () {
+    return {
+      note: {
+        backgroundImage: 'url(' + require('../../static/images/image.jpg') + ')',
+        // backgroundRepeat: 'no-repeat',
+        backgroundSize: 'auto',
+        marginTop: '0'
+      }
+    }
+  }
+}
 </script>
 
 <style>
