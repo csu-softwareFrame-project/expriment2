470427e852a9530b72b22d14e82ad706fcdf5ed9
KyrieG <2569854140@qq.com>
2020-04-24 17:53:05 +0800

    提交小版本

--- myPetStore/myPetStore-back/src/main/resources/static/js/verify.js
+++
@@ -1,6 +0,0 @@
-function verify() {
-    // 获取img元素
-    // 为了让浏览器发送请求到servlet, 所以一定要改变src
-    document.getElementById("btn").src =
-        "/useraccess/verifyCode?random=" + new Date().getTime();
-};
