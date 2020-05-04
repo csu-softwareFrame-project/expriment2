27ba558b73090f142fac60fb60121490d0fec50c
Lee-VA <a123213090@126.com>
2020-05-04 11:53:45 +0800

    添加弹窗
    原来版本

---
+++ myPetShop/src/main/resources/static/js/popwin.js
@@ -0,0 +1,7 @@
+document.getElementById('popWin').style.display="none";
+function dianwo(){
+    document.getElementById('popWin').style.display="";
+}
+function hidder(){
+    document.getElementById('popWin').style.display="none";
+}
