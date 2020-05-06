599ec9bc5eb36bb6965094bb2ddc1a45c9b72553
@zhou_jie12821948 <1670514771@qq.com>
2020-05-06 21:44:06 +0800

    完成用户的详细信息显示功能

--- myPetShop/src/main/java/org/csu/mypetstore/controller/management/ManageAccountController.java
+++ myPetShop/src/main/java/org/csu/mypetstore/controller/management/ManageAccountController.java
@@ -1,14 +1,19 @@
 package org.csu.mypetstore.controller.management;
 
+import com.alibaba.fastjson.JSONObject;
 import org.csu.mypetstore.domain.Account;
 import org.csu.mypetstore.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,5 +45,24 @@ public class ManageAccountController {
         }
         accountService.removeAccountList(usernameList2);
         return "redirect:/management/accountManagement";
+    }
+
+    @GetMapping("/getAccount")
+    @ResponseBody
+    public String getAccount(String username){
+        Account account = accountService.getAccount(username);
+        String info = account.getUsername();
+        info = info + '#' + account.getFirstName();
+        info = info + '#' + account.getLastName();
+        info = info + '#' + account.getEmail();
+        info = info + '#' + account.getPhone();
+        info = info + '#' + account.getAddress1();
+        info = info + '#' + account.getAddress2();
+        info = info + '#' + account.getCity();
+        info = info + '#' + account.getState();
+        info = info + '#' + account.getZip();
+        info = info + '#' + account.getCountry();
+        System.out.println(info);
+        return info;
     }
 }
--- myPetShop/src/main/resources/static/js/popwin.js
+++ myPetShop/src/main/resources/static/js/popwin.js
@@ -1,6 +1,9 @@
 document.getElementById('popWin').style.display="none";
-function dianwo(){
+function dianwo(e){
     document.getElementById('popWin').style.display="";
+    alert(e.currentTarget.id)
+    var accountList = "<%=session.getAttribute('accountList')%>";
+    alert(accountList)
 }
 function hidder(){
     document.getElementById('popWin').style.display="none";
