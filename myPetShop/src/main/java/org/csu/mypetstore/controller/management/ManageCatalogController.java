5355e01f0685dcff7f8b14580cfd9d6448dba24f
@zhou_jie12821948 <1670514771@qq.com>
2020-06-18 00:22:55 +0800

    完成管理端Category功能

---
+++ myPetShop/src/main/java/org/csu/mypetstore/controller/management/ManageCatalogController.java
@@ -0,0 +1,79 @@
+package org.csu.mypetstore.controller.management;
+
+import org.csu.mypetstore.domain.Category;
+import org.csu.mypetstore.domain.Item;
+import org.csu.mypetstore.domain.Product;
+import org.csu.mypetstore.service.CatalogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/management")
+public class ManageCatalogController {
+    @Autowired
+    public CatalogService catalogService;
+
+    @GetMapping("/categoryManagement")
+    public String viewCategory(Model model){
+        System.out.println("success");
+        List<Category> categoryList = catalogService.getCategoryList();
+        model.addAttribute("categoryList",categoryList);
+        return "management/categoryManagement";
+    }
+
+    @GetMapping("/viewProduct")
+    public String viewProduct(Model model,String categoryId){
+        System.out.println("success");
+        List<Product> productList = catalogService.getProductListByCategory(categoryId);
+        model.addAttribute("productList",productList);
+        return "management/productManagement";
+    }
+
+    @GetMapping("/viewItem")
+    public String viewItem(Model model,String productId){
+        List<Item> itemList = catalogService.getItemListByProductId(productId);
+        model.addAttribute("itemList",itemList);
+        return "management/itemManagement";
+    }
+
+    @GetMapping("/removeCategory")
+    public void removeCategory(String categoryList){
+        System.out.println("success");
+        System.out.println(categoryList);
+        String[] categoryIdArray = categoryList.split(",");
+        for(int i=0;i<categoryIdArray.length;i++){
+            catalogService.removeCategory(categoryIdArray[i]);
+        }
+    }
+
+    @GetMapping("/getCategory")
+    @ResponseBody
+    public String getCategory(String categoryId){
+        Category category = catalogService.getCategory(categoryId);
+        String info = category.getCategoryId();
+        info = info + "#" + category.getName();
+        return info;
+    }
+
+    @GetMapping("/updateCategory")
+    public void updateCategory(String categoryInfo){
+        String[] info = categoryInfo.split(",");
+        Category category = new Category();
+        category.setCategoryId(info[0]);
+        category.setName(info[1]);
+        if(info.length>2){
+            category.setDescription(catalogService.getCategory(info[2]).getDescription());
+            catalogService.updateCategory(category,info[2]);
+        }else{
+            category.setDescription("");
+            catalogService.insertCategory(category);
+        }
+    }
+}
